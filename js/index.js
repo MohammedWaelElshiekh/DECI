aa9ccc7d988fc127d6c9e4e86341b1c32d950572
Mohammed Wael Elshiekh <mohammedwaelelshiekhmw@gmail.com>
2024-08-08 13:52:49 +0300

    rr

--- js/index.js
+++ js/index.js
@@ -2,7 +2,7 @@
   const buttons = document.querySelectorAll(".stockElement form input");
   buttons.forEach((button) => {
     button.addEventListener("click", () => {
-      alert("works");
+      alert("done");
     });
   });
 })();
